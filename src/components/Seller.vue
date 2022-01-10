59941ce1dc83080a227b1a3dd40f8a0f18676ee5
WeiJiang9 <1079339292@qq.com>
2022-01-11 01:00:58 +0800

    实现销售图表的业务逻辑

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -11,50 +11,82 @@ export default {
     return {
       chartInstance: null,
       allData: {
-        name: [],
-        value: []
-      }
+        names: [],
+        values: []
+      },
+      currentPage: 1,
+      totalPage: 0,
+      timerId: null
     }
   },
   mounted() {
     this.getData()
     this.initChart()
   },
+  destroyed() {
+    clearInterval(this.timerId)
+  },
   methods: {
     // 初始化echarts对象
     initChart() {
       this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+      // 监听图表鼠标移入移出事件
+      this.chartInstance.on('mouseover', () => {
+        this.timerId && clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.updataChart()
+      })
     },
     async getData() {
       const { data: ret } = await require({
         url: 'api/seller'
       })
+      this.totalPage = Math.ceil(ret.length / 5)
+      // ret.sort((a, b) => {
+      //   return b.value - a.value
+      // })
       ret.forEach((item) => {
-        this.allData.name.push(item.name)
-        this.allData.value.push(item.value)
+        this.allData.names.push(item.name)
+        this.allData.values.push(item.value)
       })
+      this.setdataChart()
       this.updataChart()
     },
-    updataChart() {
+    setdataChart() {
+      let newNames = this.allData.names.slice(
+        (this.currentPage - 1) * 5,
+        this.currentPage * 5
+      )
+      let newValues = this.allData.values.slice(
+        (this.currentPage - 1) * 5,
+        this.currentPage * 5
+      )
       const option = {
-        tooltip: {
-          
-        },
+        tooltip: {},
         xAxis: {
           type: 'value'
         },
         yAxis: {
           type: 'category',
-          data: this.allData.name
+          data: newNames
         },
         series: [
           {
             type: 'bar',
-            data: this.allData.value
+            data: newValues
           }
         ]
       }
       this.chartInstance.setOption(option)
+    },
+    updataChart() {
+      this.timerId && clearInterval(this.timerId)
+      this.timerId = setInterval(() => {
+        this.currentPage++
+        if (this.currentPage > this.totalPage) this.currentPage = 1
+        this.setdataChart()
+      }, 3000)
     }
   }
 }
